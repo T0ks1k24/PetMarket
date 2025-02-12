6e256b1ab6e05de0c13768fc0a9d86fb317d366c
Toksik <toksamal2004@gmail.com>
2025-02-13 01:48:42 +0200

    Push
    Add Models Category and Product

---
+++ Backend/Data/Models/CategoryEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.Data.Models
+{
+    public class CategoryEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ Backend/Data/Models/ProductEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend.Data.Models
+{
+    public class ProductEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Price { get; set; }
+        public string ImageUrl {  get; set; } = string.Empty;
+
+        public string CaaId { get; set; }
+    }
+}
